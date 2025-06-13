457f19172791e1458431f80a2cb7a8a8e9a0bd2f
Oladeji Omolabake <oladejiomolabake14@Gmail.com>
2025-06-13 20:14:15 +0100

    second commit

--- app/Components/SchoolAdminDashBoard/BroadSheet.jsx
+++ app/Components/SchoolAdminDashBoard/BroadSheet.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const students = Array(10).fill({
-  name: "Babalola Ife",
+  name: "Babalola Ifeoluwa",
   subjects: {
     Maths: 70,
     English: 80,
@@ -17,16 +17,17 @@ const students = Array(10).fill({
 });
 const BroadSheet = () => {
   return (
-    <div className="overflow-x-auto p-4 px-0 ">
-      <table className="min-w-full border border-[#01427A]/15 text-sm">
+    <div className="overflow-x-auto w-full">
+      <table className="w-max min-w-full border border-[#01427A]/15 text-sm">
         <thead className="bg-[#f8fdff] text-gray-800">
           <tr>
             <th
-              className="border  px-3 py-2 text-center text-[#6DAAD3] rotate-[300deg]"
+              className="border px-8 py-2 text-center text-[#6DAAD3]  sticky left-0 bg-[#f8fdff] z-10"
               rowSpan={2}
             >
-              NAMES
+              <div className="rotate-[300deg]">NAMES</div>
             </th>
+
             <th
               className="border px-3 py-4 font-normal bg-[#6B90B5]/4"
               colSpan={12}
@@ -35,7 +36,7 @@ const BroadSheet = () => {
             </th>
           </tr>
 
-          <tr className="bg-[#6B90B5]/4 text-xs">
+          <tr className="bg-[#6B90B5]/4 ">
             <th className="border px-3 py-2">Maths (100)</th>
             <th className="border px-3 py-2">English (30)</th>
             <th className="border px-3 py-2">Chemistry (30)</th>
@@ -49,39 +50,47 @@ const BroadSheet = () => {
             <th className="border px-3 py-2">Comment</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="text-sm">
           {students.map((student, idx) => (
             <tr key={idx} className="text-center font-medium">
-              <td className="border px-5 py-1 font-medium  text-left">
+              <td
+                title={student.name}
+                className="border px-3 py-3 font-medium truncate cursor-pointer text-left sticky left-0 bg-white z-10"
+              >
                 {student.name}
               </td>
-              <td className="border px-3 text-[#333333]">
+
+              <td className="border px-3 py-3 text-[#333333]">
                 {student.subjects.Maths}
               </td>
-              <td className="border px-3 text-[#333333] ">
+              <td className="border px-3 py-3 text-[#333333] ">
                 {student.subjects.English}
               </td>
-              <td className="border px-3 text-[#333333] ">
+              <td className="border px-3 py-3 text-[#333333] ">
                 {student.subjects.Chemistry}
               </td>
-              <td className="border px-3 text-[#333333] ">
+              <td className="border px-3 py-3 text-[#333333] ">
                 {student.subjects.Physics}
               </td>
-              <td className="border px-3 text-[#333333] ">
+              <td className="border px-3 py-3 text-[#333333] ">
                 {student.subjects.Biology}
               </td>
-              <td className="border px-3 text-[#333333] ">
+              <td className="border px-3 py-3 text-[#333333] ">
                 {student.subjects.Agric}
               </td>
-              <td className="border px-3 text-[#333333] ">
+              <td className="border px-3 py-3 text-[#333333] ">
                 {student.subjects.Yoruba}
               </td>
-              <td className="border px-3 text-[#333333] ">
+              <td className="border px-3 py-3 text-[#333333] ">
                 {student.subjects.Geography}
               </td>
-              <td className="border px-3 text-[#333333] ">{student.passed}</td>
-              <td className="border px-3 text-[#333333] ">{student.failed}</td>
-              <td className="border px-3  bg-[#89B3DF]/60">
+              <td className="border px-3 py-3 text-[#333333] ">
+                {student.passed}
+              </td>
+              <td className="border px-3 py-3 text-[#333333] ">
+                {student.failed}
+              </td>
+              <td className="border px-3 py-3  bg-[#89B3DF]/60">
                 <input
                   type="text"
                   placeholder="Type a Comment"
